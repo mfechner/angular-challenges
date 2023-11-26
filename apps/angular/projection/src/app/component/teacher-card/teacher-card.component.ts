720d9603d7d8c3beff74a532e94944a8eee5ece3
Matthias Fechner <matthias@fechner.net>
2023-11-26 11:57:17 +0200

    Used content projection to more commonly use the card component and also inject the item-line via content projection

--- apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,16 +1,24 @@
+import { NgIf, AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import { FakeHttpService, randTeacher } from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
-import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
-  template: `<app-card
-    [list]="teachers"
-    [type]="cardType"
-    customClass="bg-light-red"></app-card>`,
+  template: `
+    <app-card
+      [list]="teachers$ | async"
+      (add)="addTeacher()"
+      class="bg-light-red">
+      <img src="assets/img/teacher.png" width="200px"/>
+      <ng-template #rowRef let-teacher>
+        <app-list-item (delete)="deleteTeacher(teacher.id)">
+          {{ teacher.firstname }}
+        </app-list-item>
+      </ng-template>
+    </app-card>`,
   styles: [
     `
       .bg-light-red {
@@ -19,17 +27,23 @@ import { CardComponent } from '../../ui/card/card.component';
     `,
   ],
   standalone: true,
-  imports: [CardComponent],
+  imports: [ CardComponent, NgIf, AsyncPipe, ListItemComponent ],
 })
 export class TeacherCardComponent implements OnInit {
-  teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
+  teachers$ = this.store.teachers$;
 
-  constructor(private http: FakeHttpService, private store: TeacherStore) {}
+  constructor(private http: FakeHttpService, private store: TeacherStore) {
+  }
 
   ngOnInit(): void {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
+  }
 
-    this.store.teachers$.subscribe((t) => (this.teachers = t));
+  addTeacher() {
+    this.store.addOne(randTeacher());
+  }
+
+  deleteTeacher(id: number) {
+    this.store.deleteOne(id);
   }
 }
